68a8aba65499f78f086e0447d0eec088bfee4417
IsaiasRVH2 <isaias.valdivia@uaz.edu.mx>
2025-08-01 18:50:32 -0600

    feat: :sparkles: Creada una función para agregar un amigo a la lista
    Se creó una función que agrega un amigo a la lista. Primero se obtiene el nombre del input con id amigo, se valida que no este vacio y en caso de no estarlo se agrega a la lista y se limpia el campo de texto. En caso de que el input este vacio se manda un alert indicando que se debe ingresar un nombre.

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    if (validarNombre(nombre)) {
+        amigos.push(nombre);
+        document.getElementById("amigo").value = ""; //Limpiar el campo de entrada
+    } else {
+        alert("Por favor, inserte un nombre.");
+    }
+}
+
+function validarNombre(nombre) {
+    return nombre.trim() !== "";
+}
